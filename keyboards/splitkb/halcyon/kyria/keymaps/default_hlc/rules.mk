ff91c7d176a34c6e6659c2b10bb1cc70436b5257
Jimmie Högklint <hogklint@gmail.com>
2026-06-01 08:12:04 +0200

    Update to QMK mouse key codes

--- keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/rules.mk
+++ keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/rules.mk
@@ -1,6 +1,7 @@
 ENCODER_MAP_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
+MOUSEKEY_ENABLE = yes
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
